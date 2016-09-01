cb236fe871df4b1534678c91f14d5ed352573c9e
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-09-01 18:43:46 +0700

    [Bugfix] Don't skip root node if it's the only result

--- lib/App/jsonsel.pm
+++ lib/App/jsonsel.pm
@@ -60,12 +60,14 @@ sub jsonsel {
 
     require Data::CSel::WrapStruct;
     my $tree = Data::CSel::WrapStruct::wrap_struct($data);
+    use DD; dd $tree;
 
     my @matches = Data::CSel::csel(
         {class_prefixes=>['Data::CSel::WrapStruct']}, $expr, $tree);
 
-    # skip root node itself
-    @matches = grep { refaddr($_) ne refaddr($tree) } @matches;
+    # skip root node itself to avoid duplication
+    @matches = grep { refaddr($_) ne refaddr($tree) } @matches
+        unless @matches <= 1;
 
     for my $action (@$actions) {
         if ($action eq 'print') {
